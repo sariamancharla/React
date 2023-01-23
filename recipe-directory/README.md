742babe2a6614a9027008b4f60c156fe3e91f316
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2023-01-23 19:51:12 +0100

    Submit to db.json file
    Added code to set the option for post data to json
    Added CSS for create component
    Added code for create recipe with more than one ingredient

--- recipe-directory/README.md
+++ recipe-directory/README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+https://github.com/iamshaunjp/React-Firebase/blob/lesson-87/cooking-ninja/src/pages/create/Create.css
--- recipe-directory/src/pages/create/Create.js
+++ recipe-directory/src/pages/create/Create.js
@@ -1,10 +1,81 @@
-// styles
-import './Create.css'
-
-import React from 'react'
+import { useRef, useState } from "react"
+import { useFetch } from "../../hooks/useFetch"
+import "./Create.css"
 
 export default function Create() {
+  const [title,setTitle] =useState('')
+  const [method,setMethod] =useState('')
+  const [cookingTime,setCookingTime] =useState('')
+  const [newIngredient,setNewIngredient]=useState('')
+  const [ingredients,setIngredients]=useState([])
+  const ingredientInput=useRef(null)
+  const{data,error,postData}=useFetch('http://localhost:8000/recipes',"POST")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title,method,cookingTime,ingredients)
+    postData({title,ingredients,method,cookingTime:cookingTime+' minutes'})
+  }
+
+  const handleAdd=(e)=>{
+    e.preventDefault()
+    const ing=newIngredient.trim()
+    if (ing && !ingredients.includes(ing)){
+        setIngredients(prevIngredients=>[...prevIngredients,ing])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
+  }
+
   return (
-    <div>Create</div>
+    <div className ="create">
+      <h2 className="page-title">Add a new Recipe</h2>
+       <form onSubmit ={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input 
+            type ="text"
+            onChange ={(e)=>setTitle(e.target.value)}
+            value ={title}
+            required
+          />
+        </label>
+        
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input type="text" 
+            onChange={(e)=>setNewIngredient(e.target.value)}
+            value={newIngredient}
+            ref={ingredientInput}            
+            />
+            <button onClick={handleAdd} className="btn">Add</button>
+          </div>
+        </label>
+        
+        <p>Current Ingredients: {ingredients.map(i=><em key={i}>{i},</em>)}</p> 
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            onChange ={(e)=>setMethod(e.target.value)}
+            value ={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time(minutes):</span>
+          <input
+            onChange ={(e)=>setCookingTime(e.target.value)}
+            value ={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+       </form>
+      
+
+    </div>
   )
 }
